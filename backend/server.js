2f499b2f9eb6450d1c1c239cdc5a65f489fe1d8a
anissalopez <anissa965@gmail.com>
2023-08-09 10:01:14 -0400

    created react app

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,25 @@
 const express = require('express');
-const habitList = require("./data");
+const habitList = require("./habitList");
 const dotenv = require('dotenv');
-
-
 dotenv.config();
-
+const PORT = process.env.PORT  || 4000
 const app = express();
+
 
 app.get("/", (req, res) => {
     res.send("API is running...")
 });
 
 
-app.get("/data", (req, res) => {
+app.get("/habitList", (req, res) => {
     res.json(habitList)
 })
 
-app.get("/data/:id", (req, res) =>{
+app.get("/habitList/:id", (req, res) =>{
     const habit = habitList.find((n) => n.id === req.params.id)
     res.send(habit)
 })
 
-const PORT = process.env.PORT  || 4000
 
+console.log(module)
 app.listen(PORT, ()=>{console.log(`app is listening on ${PORT}`)})
