ae7e2d44a2f16d70d6fdd219c30dcaa48c99c043
anissalopez <anissa965@gmail.com>
2023-08-11 10:46:29 -0400

    completed registration api

--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import bcrypt from bycryptjs;
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema(
     {
@@ -31,9 +31,11 @@ const userSchema = mongoose.Schema(
     if (!this.isModified("password")) {
       next();
     }
-    const salt = await bcrypt.genSalt(10);
+    const salt = await bcrypt.genSalt(8);
     this.password = await bcrypt.hash(this.password, salt);
   });
+
+
   
   const User = mongoose.model("User", userSchema);
   
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js"
+import { notFound, errorHandler} from "./middlewares/errorMiddleware.js";
+
 
 
 
@@ -16,20 +18,13 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/data/users', userRoutes)
+app.use(notFound)
+app.use(errorHandler)
 
 
 app.get("/", (req, res) => {
     res.send("API is running...")
 });
-
-
-
-
-
-
-
-
-
 
 const PORT = process.env.PORT  || 4000
 
